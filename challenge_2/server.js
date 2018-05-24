73edb1828ca441059f507f196d58755cf2788763
Moyeong Lee <2000prcs@gmail.com>
2018-05-24 11:02:38 -0700

    Initial setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
 
+const port = process.env.PORT || 7777;
+
+const parser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(parser());
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+})
 
---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client')
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
